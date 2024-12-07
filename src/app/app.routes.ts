ce0395faed2482b5528679dc54e68c1280387b24
Hugo Cortez <hugocortezprog@gmail.com>
2024-12-07 18:54:41 -0300

    feat: creating components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
+import { authRoutes } from './features/authentication/auth.routes';
+import { LoginComponent } from './features/authentication/components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [authRoutes];
