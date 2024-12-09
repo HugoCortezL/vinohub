fd2f100eab70f5008d5b9533a84b046e550f6280
Hugo Cortez <hugocortezprog@gmail.com>
2024-12-09 15:57:16 -0300

    adding register layout

--- src/app/features/authentication/components/register/register.component.ts
+++ src/app/features/authentication/components/register/register.component.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ButtonComponent } from "../../../../shared/components/button/button.component";
+import { IconComponent } from "../../../../shared/components/icon/icon.component";
+import { InputTextComponent } from "../../../../shared/components/input-text/input-text.component";
+import { AuthRoutesNames } from "../../auth.routes";
 
 @Component({
   selector: "app-register",
-  imports: [],
+  imports: [CommonModule, RouterModule, InputTextComponent, IconComponent, ButtonComponent],
   templateUrl: "./register.component.html",
   styleUrl: "./register.component.scss",
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  authRoutes = AuthRoutesNames;
+}
